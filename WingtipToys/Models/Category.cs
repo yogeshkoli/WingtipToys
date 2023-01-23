eefdab4b6493be938c71f2911067bffbaf096a49
Yogesh Koli <yogeshkoli@hotmail.com>
2023-01-23 17:23:59 +0530

    Added a Context Class

--- WingtipToys/Models/Category.cs
+++ WingtipToys/Models/Category.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace WingtipToys.Models
 {
     public class Category
     {
+        [ScaffoldColumn(false)]
+        public int CategoryId { get; set; }
+
+        [Required, StringLength(100), Display(Name = "Name")]
+        public string CategoryName { get; set; }
+
+        [Display(Name = "Category Description")]
+        public string Description { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
     }
 }
