08a7c3711bca236bb85787364b18c8323f077782
Yogesh Koli <yogeshkoli@hotmail.com>
2023-01-24 17:48:17 +0530

    Using a ASP List View and redering the categories

--- WingtipToys/Site.Master.cs
+++ WingtipToys/Site.Master.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using System.Security.Principal;
 using System.Web;
@@ -7,6 +8,7 @@ using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Microsoft.AspNet.Identity;
+using WingtipToys.Models;
 
 namespace WingtipToys
 {
@@ -70,6 +72,13 @@ namespace WingtipToys
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public IQueryable<Category> GetCategories()
+        {
+            var _db = new WingtipToys.Models.ProductContext();
+            IQueryable<Category> query = _db.Categories;
+            return query;
         }
 
         protected void Unnamed_LoggingOut(object sender, LoginCancelEventArgs e)
