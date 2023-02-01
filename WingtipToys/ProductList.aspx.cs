4cf493a0b082865aae4b1ebea82eb5a87d03b993
Yogesh Koli <yogeshkoli@hotmail.com>
2023-02-01 11:00:29 +0530

    feat: product list
    add product listing

--- WingtipToys/ProductList.aspx.cs
+++ WingtipToys/ProductList.aspx.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Web.ModelBinding;
+using WingtipToys.Models;
 
 namespace WingtipToys
 {
@@ -12,6 +10,18 @@ namespace WingtipToys
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public IQueryable<Product> GetProducts([QueryString("id")] int? categoryId)
+        {
+            var _db = new WingtipToys.Models.ProductContext();
+            IQueryable<Product> query = _db.Products;
+            if(categoryId.HasValue && categoryId > 0)
+            {
+                query = query.Where(p => p.CategoryID== categoryId);
+            }
+
+            return query;
         }
     }
 }
